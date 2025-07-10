a86e0d8ef7ced548f3634fbc80e797ac7e0ff5b4
HDNghia <huynhdangnghia68@gmail.com>
2025-07-10 16:36:10 +0700

    rollback

--- src/graphics/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Logo: any = () => (
-    <div className="Logo">
-        <img src="https://i.postimg.cc/pr2L0BrY/android-chrome-512x512.png" width="200" height="200" alt="igLab Logo" />
-    </div>
-);
