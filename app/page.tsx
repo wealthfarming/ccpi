1061413a3af8678d71c496e36d4b1ce1dec9f208
HDNghia <huynhdangnghia68@gmail.com>
2025-07-10 17:18:59 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import menuData from '@/data/menu.json';
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuLink,
+} from '@/components/ui/navigation-menu';
+
+function RenderMenuItems({ items, depth = 0 }: { items: any[]; depth?: number }) {
+  return items.map((item, index) => (
+    <div key={`${item.title.en}-${index}`} style={{ paddingLeft: depth * 10 }}>
+      {item.children.length > 0 ? (
+        <>
+          <NavigationMenuTrigger>{item.title.en}</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div className="p-2">
+              <RenderMenuItems items={item.children} depth={depth + 1} />
+            </div>
+          </NavigationMenuContent>
+        </>
+      ) : (
+        <NavigationMenuLink
+          href={item.url}
+          target={item.open_in_new_tab ? '_blank' : undefined}
+        >
+          {item.title.en}
+        </NavigationMenuLink>
+      )}
+    </div>
+  ));
+}
+
+export default function Home() {
+  const menuItems = menuData.menu_items;
+
+  return (
+    <div className="home">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {menuItems.map((item, index) => (
+            <NavigationMenuItem key={`${item.title.en}-${index}`}>
+              {item.children.length > 0 ? (
+                <>
+                  <NavigationMenuTrigger>{item.title.en}</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <div className="p-2">
+                      <RenderMenuItems items={item.children} depth={1} />
+                    </div>
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <NavigationMenuLink
+                  href={item.url}
+                  // target={item.open_in_new_tab ? '_blank' : undefined}
+                >
+                  {item.title.en}
+                </NavigationMenuLink>
+              )}
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
