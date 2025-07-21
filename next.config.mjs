4f92b75263ed2ca011e03a5138012e75dc05dd75
HDNghia <huynhdangnghia68@gmail.com>
2025-07-21 14:04:34 +0700

    fix css

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-};
-
-export default nextConfig;
+    output: 'export',
+    basePath: process.env.BASE_PATH || '',  // Use env var for dynamic basePath
+    assetPrefix: process.env.BASE_PATH || '',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
