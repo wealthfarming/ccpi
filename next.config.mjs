29e5a7823c259fd3342e511e458718f548fb8805
HDNghia <huynhdangnghia68@gmail.com>
2025-08-01 19:51:38 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    basePath: process.env.BASE_PATH || '',  // Use env var for dynamic basePath
-    assetPrefix: process.env.BASE_PATH || '',
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+};
+
+export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
   theme: {
-    extend: {
-      fontFamily: {
-        'montserrat': ['var(--font-montserrat)', 'sans-serif'],
-        'open-sans': ['var(--font-open-sans)', 'sans-serif'],
-        'roboto': ['var(--font-roboto)', 'sans-serif'],
-        'roboto-condensed': ['var(--font-roboto-condensed)', 'sans-serif'],
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+    fontFamily: {
+      'montserrat': ['var(--font-montserrat)', 'sans-serif'],
+      'open-sans': ['var(--font-open-sans)', 'sans-serif'],
+      'roboto': ['var(--font-roboto)', 'sans-serif'],
+      'roboto-condensed': ['var(--font-roboto-condensed)', 'sans-serif'],
     },
   },
 };
